2643be086042fa80c1fe6556a3c1b468d59da3ba
jklj077 <jklj077@gmail.com>
2020-01-03 11:38:53 +0800

    backport

--- fairseq/tasks/language_modeling.py
+++ fairseq/tasks/language_modeling.py
@@ -14,6 +14,7 @@ from fairseq.data import (
     MonolingualDataset,
     TokenBlockDataset,
     TransformEosDataset,
+    TruncateDataset,
     TruncatedDictionary,
 )
 from fairseq.tasks import FairseqTask, register_task
@@ -79,6 +80,8 @@ class LanguageModelingTask(FairseqTask):
                             help='prepend beginning of sentence token (<s>)')
         parser.add_argument('--max-target-positions', type=int, metavar='N',
                             help='max number of tokens in the target sequence')
+        parser.add_argument('--truncate-sequence', action='store_true', default=False,
+                            help='truncate sequences to --tokens-per-sample')
         # fmt: on
 
     def __init__(self, args, dictionary, output_dictionary=None, targets=None):
@@ -168,6 +171,9 @@ class LanguageModelingTask(FairseqTask):
             raise FileNotFoundError(
                 "Dataset not found: {} ({})".format(split, split_path)
             )
+
+        if self.args.truncate_sequence:
+            dataset = TruncateDataset(dataset, self.args.tokens_per_sample)
 
         dataset = TokenBlockDataset(
             dataset,
